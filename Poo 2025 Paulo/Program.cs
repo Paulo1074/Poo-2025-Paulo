cfd719477876d42e9459b58ca67c675456e9840b
Paulo Otávio <pereirapaulootavio98@gmail.com>
2025-06-03 20:51:40 -0300

    Adicionar arquivos de projeto.

---
+++ Poo 2025 Paulo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Poo_2025_Paulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
